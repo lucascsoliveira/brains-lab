1d688242679f126f432d549cc1bee6a6eaab7341
Lucas Oliveira <lucascs.oliveira@gmail.com>
2015-06-26 18:52:27 -0300

    Add método clone()
    O método clone foi sobrescrito na classe Posicao.

--- src/main/java/so/coutinho/lucas/brainslab/Posicao.java
+++ src/main/java/so/coutinho/lucas/brainslab/Posicao.java
@@ -4,10 +4,24 @@ package so.coutinho.lucas.brainslab;
  *
  * @author Lucas
  */
-public class Posicao extends Par<Integer, Integer> {
+public class Posicao extends Par<Integer, Integer> implements Cloneable {
 
     public Posicao(Integer x, Integer y) {
         super(x, y);
+    }
+
+    @Override
+    @SuppressWarnings("CloneDeclaresCloneNotSupported")
+    public Posicao clone() {
+        try {
+            return (Posicao) super.clone();
+        } catch (CloneNotSupportedException ex) {
+            /**
+             * A exceção CloneNotSupportedException nunca será lançada devido a
+             * implementação da interface Cloneable.
+             */
+            throw new RuntimeException("Clone fail...");
+        }
     }
 
 }
